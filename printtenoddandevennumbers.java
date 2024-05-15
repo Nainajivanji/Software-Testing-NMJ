32e5e970bb684c5fda4f68f0463d96e930ed5376
Nainajivanji <167822064+Nainajivanji@users.noreply.github.com>
2024-05-15 20:55:37 +0100

    printing 10 odd and 10 even numbers using For Loop
    Assignment:
    For Loop:
    2. Write a program to print 10 even numbers and 10 odd numbers.

---
+++ printtenoddandevennumbers.java
@@ -0,0 +1,26 @@
+package forloop;
+
+public class printtenoddandevennumbers {
+    public static void main(String[] args) {
+
+        int n = 20;
+        for (int i = 2; i <= n; i++) {
+
+            if (i % 2 == 0) {
+                   System.out.println("Even number");
+                System.out.println(i);
+            }
+                if (i % 2 != 0) {
+                   System.out.println("Odd number");
+                    System.out.println(i);
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+
