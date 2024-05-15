558f1b6560b0900204928b851da30336107ce84e
Nainajivanji <167822064+Nainajivanji@users.noreply.github.com>
2024-05-15 15:42:00 +0100

    Using for loop in multiplication table
    Assignment:
    3. Write a program to generate tables of 10.

---
+++ tableoften.java
@@ -0,0 +1,15 @@
+package forloop;
+
+public class tableoften {
+    public static void main(String[] args) {
+        int N = 10;
+        for (int i = 1; i <= 10; ++i) {
+
+
+            System.out.println(N + "*" + i + "=" + N * i);
+
+        }
+    }
+}
+
+
