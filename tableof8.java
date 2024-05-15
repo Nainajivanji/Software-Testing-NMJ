2a39c58f4adbb9a46a4f6da7b3adfa06d6cdee67
Nainajivanji <167822064+Nainajivanji@users.noreply.github.com>
2024-05-15 18:24:56 +0100

    Using while loop only multiplication table of 8
    Assignment:
    Do While Loop:
    7. Write a program to generate tables of eg 8 .

---
+++ tableof8.java
@@ -0,0 +1,16 @@
+package multiplicationtableusingwhileloop;
+
+public class tableof8 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 1;
+            while (b <= 12 ) {
+            System.out.println(a + "*" + b + "=" + a * b);
+            b++;
+            {
+
+
+            }
+        }
+    }
+}
